3d06d7963675e08ba8f55bbfc7e3441f2c64bb7d
AlejandroMG1 <alejandrom.montoya@udea.edu.co>
2024-01-01 14:57:22 -0500

    intial setup

---
+++ node/tests/example.test.ts
@@ -0,0 +1,10 @@
+/* eslint-disable jest/expect-expect */
+import { assert } from 'chai'
+
+describe('Calculator Tests', () => {
+  it('should return 5 when 2 is added to 3', () => {
+    const result = 2 + 5
+
+    assert.equal(result, 5)
+  })
+})
