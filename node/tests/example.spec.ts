88a6982834c24a566229f9b34f27515a3f498e88
Alejandro <alejandrom.montoya@udea.edu.co>
2024-01-07 15:01:23 -0500

    esm

---
+++ node/tests/example.spec.ts
@@ -0,0 +1,16 @@
+/* eslint-disable jest/expect-expect */
+import { OrderDetailResponse } from '@vtex/clients'
+import {calculateValuePerCategory} from '../helpers/calculateCategoryValue'
+import orders from '../test_tools/prebuildMoks/orders.json'
+import { assert } from 'chai'
+
+
+describe('Calculator Tests', () => {
+  it('should return 5 when 2 is added to 3', () => {
+    const [order] = (orders as unknown) as OrderDetailResponse[]
+
+    const result = calculateValuePerCategory(22, order.items)
+    result
+    assert.equal(result, 5)
+  })
+})
